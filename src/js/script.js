8d14b8052a19bd535033e9f78ad4f001a9d0257a
djedek <ex@gmail.com>
2020-10-05 21:33:47 +0300

    Блок solutions с адаптацией под различные разрешения

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,20 @@ document.querySelector('.prev').addEventListener('click', function (){
 document.querySelector('.next').addEventListener('click', function (){
 	slider.goTo('next');
 });
+
+const slider_mini = tns({
+    container: '.carousel-mini__my-slider',
+    items: 1,
+    slideBy: 'page',
+	autoplay: false,
+	controls: false,
+	navPosition: 'bottom'
+});
+
+document.querySelector('.prev_mini').addEventListener('click', function (){
+	slider_mini.goTo('prev');
+});
+
+document.querySelector('.next_mini').addEventListener('click', function (){
+	slider_mini.goTo('next');
+});
