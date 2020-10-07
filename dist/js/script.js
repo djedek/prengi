c757e56acd9f52f2d134dfaf61aa42ec5d4ac8de
djedek <ex@gmail.com>
2020-10-07 19:35:24 +0300

    Исправления

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,11 +9,13 @@ $(function(){
 		}
 	});
 
-	$("a[href^='#']").click(function(){
+	$("a[href='#up']").click(function(){
 		var _href = $(this).attr("href");
 		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
 		return false;
 	});
+
+	
 
 });
 
